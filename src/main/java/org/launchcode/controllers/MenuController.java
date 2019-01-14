fdc55d7f43f7773fbb2e442865082642a3e43df3
Bradley Boggs <bradleywboggs@gmail.com>
2019-01-14 15:40:48 -0600

    Adding Cheeses to menus feature is functional
    CONTROLLER/VIEWS - Add appropriate handlers/views for viewing all Menus-menus/index.html , viewing an individual menu-menu/view.html, and adding a new cheese to that menu-menu/add-item.html. Use Data Access Objects to save to DB.
    
    MODELS - Create an AddMenuItemForm model form to pass into the view(menu/add-item.html) and validate data.

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Cheese;
 import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
+import org.launchcode.models.forms.AddMenuItemForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,6 +30,15 @@ public class MenuController {
         return "menu/index";
     }
 
+    @GetMapping("view")
+    public String viewMenu(Model model, @RequestParam int id) {
+        Menu menu = menuDao.findOne(id);
+        model.addAttribute("title", "Menu: " + menu.getName());
+        model.addAttribute("menu", menu);
+        return "menu/view";
+    }
+
+
     @GetMapping("add")
     public String displayAddMenuForm(Model model) {
         model.addAttribute("title", "Add a Menu");
@@ -42,12 +53,37 @@ public class MenuController {
             return "menu/add";
         } else {
             menuDao.save(newMenu);
-            return "redirect:";
+            return "redirect:view?id=" + newMenu.getId();
         }
-
-
-
 
     }
 
-}
+    @GetMapping("add-item")
+    public String displayAddMenuItemForm(Model model, @RequestParam int id) {
+        Menu menu = menuDao.findOne(id);
+        Iterable<Cheese> cheeses = cheeseDao.findAll();
+        model.addAttribute("title", "Add Cheeses to Menu: " + menu.getName());
+        model.addAttribute("form", new AddMenuItemForm(menu, cheeses));
+        return "menu/add-item";
+
+    }
+
+    @PostMapping("add-item")
+    public String processAddMenuItemForm(Model model, @RequestParam int id, @Valid AddMenuItemForm form,
+                                         Errors errors)   {
+       Menu menu = menuDao.findOne(id);
+       Cheese newCheese = cheeseDao.findOne(form.getCheeseId());
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Cheeses to Menu: " + menu.getName());
+            model.addAttribute("form", form);
+            return "menu/add-item";
+         } else {
+                menu.addItem(newCheese);
+                menuDao.save(menu);
+                return "redirect:/menu/view?id=" + id;
+            }
+        }
+
+    }
+
+
